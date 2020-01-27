edb41c4fd9d3988f13906110dd17590981874577
orchaland <orchaland@gmail.com>
2020-01-27 12:00:48 +0100

    first

---
+++ src/app/personne/personne.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators, FormsModule} from '@angular/forms';
+
+import { Person } from './person';
+
+@Component({
+  selector: 'app-personne',
+  templateUrl: './personne.component.html',
+  styleUrls: ['./personne.component.css']
+})
+export class PersonneComponent implements OnInit {
+
+  checkoutForm;
+  persons: Person[]  = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+          name: '',
+          age: ''
+        });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(customerData) {
+      // Process checkout data here
+      console.warn('Your order has been submitted', customerData);
+      this.persons.push(customerData);
+      this.checkoutForm.reset();
+    }
+
+}
