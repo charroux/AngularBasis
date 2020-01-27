ad202ccecad9ffa95e04c4f1b6b71da9041ac735
orchaland <orchaland@gmail.com>
2020-01-27 11:59:07 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PersonneComponent } from './personne/personne.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
